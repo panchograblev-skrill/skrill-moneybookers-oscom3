c24c6ce748f03771b2b1009c801a2936f0370326
Pancho Grablev <pancho.grablev@skrill.com>
2011-11-17 18:10:46 +0200

    fixes

--- oscommerce/osCommerce/OM/Core/Site/Shop/Application/MBStatus/Controller.php
+++ oscommerce/osCommerce/OM/Core/Site/Shop/Application/MBStatus/Controller.php
@@ -1,21 +1,15 @@
 <?php
-/**
- * osCommerce Online Merchant
- * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
- * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
- */
 
-  namespace osCommerce\OM\Core\Site\Shop\Application\Checkout\Action\MBPayment;
+namespace osCommerce\OM\Core\Site\Shop\Application\MBStatus;
 
-  use osCommerce\OM\Core\ApplicationAbstract;
-  use osCommerce\OM\Core\Registry;
-  use osCommerce\OM\Core\OSCOM;
-  use osCommerce\OM\Core\Site\Shop\PaymentModuleAbstract;
+use osCommerce\OM\Core\Site\Shop\Order;
 
-  class Status {
-    public static function execute(ApplicationAbstract $application) {
-      
+class Controller extends \osCommerce\OM\Core\Site\Shop\ApplicationAbstract
+{
+    protected function initialize() { }
+
+    protected function process()
+    {
         $merchant_sig = $_POST['merchant_id'] . 
                         $_POST['transaction_id'] . 
                         strtoupper(md5( MODULE_PAYMENT_MONEYBOOKERS_SECRET_WORD )) . 
@@ -26,11 +20,10 @@
 	$merchant_sig = strtoupper(md5($merchant_sig));
 	$mb_sig = isset($_POST['md5sig']) ? $_POST['md5sig'] : '';
 	
-	if ( $mb_sig === $merchant_sig && $_POST['status'] == '2' )
+	if ( ($mb_sig === $merchant_sig ) && $_POST['status'] == '2' )
             Order::process($_POST['transaction_id'], MODULE_PAYMENT_MONEYBOOKERS_ORDER_STATUS_ID);
         
         file_put_contents("status.txt", print_r($_POST, true), FILE_APPEND);
-        
     }
-  }
-?>
+
+}
