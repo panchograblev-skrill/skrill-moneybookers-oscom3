3819955ebd6419947db84da293b6a38cbb20fe77
Pancho Grablev <pancho.grablev@skrill.com>
2011-11-17 18:31:31 +0200

    fixes

--- oscommerce/osCommerce/OM/Core/Site/Shop/Application/MBStatus/Controller.php
+++ oscommerce/osCommerce/OM/Core/Site/Shop/Application/MBStatus/Controller.php
@@ -19,9 +19,12 @@ class Controller extends \osCommerce\OM\Core\Site\Shop\ApplicationAbstract
 		
 	$merchant_sig = strtoupper(md5($merchant_sig));
 	$mb_sig = isset($_POST['md5sig']) ? $_POST['md5sig'] : '';
+        
+        $order_status = ( constant('MODULE_PAYMENT_MONEYBOOKERS_ORDER_STATUS_ID') > 0 ) ? 
+                            MODULE_PAYMENT_MONEYBOOKERS_ORDER_STATUS_ID : null;
 	
 	if ( ($mb_sig === $merchant_sig ) && $_POST['status'] == '2' )
-            Order::process($_POST['transaction_id'], MODULE_PAYMENT_MONEYBOOKERS_ORDER_STATUS_ID);
+            Order::process($_POST['transaction_id'], $order_status  ) ;
         
         file_put_contents("status.txt", print_r($_POST, true), FILE_APPEND);
     }
