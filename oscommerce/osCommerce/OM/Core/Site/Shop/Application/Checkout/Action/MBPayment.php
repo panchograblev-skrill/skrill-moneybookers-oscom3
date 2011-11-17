9389ba1beb1ae3d053515d43ef4b74d363f52678
Pancho Grablev <pancho.grablev@skrill.com>
2011-11-17 18:55:44 +0200

    fixes

--- oscommerce/osCommerce/OM/Core/Site/Shop/Application/Checkout/Action/MBPayment.php
+++ oscommerce/osCommerce/OM/Core/Site/Shop/Application/Checkout/Action/MBPayment.php
@@ -1,22 +1,21 @@
 <?php
+
+namespace osCommerce\OM\Core\Site\Shop\Application\Checkout\Action;
+
+use osCommerce\OM\Core\ApplicationAbstract;
+use osCommerce\OM\Core\Registry;
+
 /**
- * osCommerce Online Merchant
- * 
- * @copyright Copyright (c) 2011 osCommerce; http://www.oscommerce.com
- * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ * @author Pancho Grablev <pancho.grablev@skrill.com>
  */
+class MBPayment {
 
-  namespace osCommerce\OM\Core\Site\Shop\Application\Checkout\Action;
-
-  use osCommerce\OM\Core\ApplicationAbstract;
-
-  /**
-   * @author Pancho Grablev <pancho.grablev@skrill.com>
-   */
-  class MBPayment {
     public static function execute(ApplicationAbstract $application) {
 
-      $application->setPageContent('mb_payment.php');
+        // delete the shopping cart
+        $OSCOM_ShoppingCart = Registry::get('ShoppingCart');
+        $OSCOM_ShoppingCart->reset(true);
+
+        $application->setPageContent('mb_payment.php');
     }
-  }
-?>
+}
